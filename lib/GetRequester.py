71af3906310f129dd93d94cd3ec2ed95c9dd6ae2
Lacey Murphy <lneagle@gmail.com>
2025-09-09 11:25:14 -0400

    Implemented get_response_body and load_json

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        return requests.get(self.url).content
 
     def load_json(self):
-        pass
+        return requests.get(self.url).json()
+    
+if __name__ == '__main__':
+    URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+
+    print(GetRequester(URL).get_response_body())
+    print(GetRequester(URL).load_json())
